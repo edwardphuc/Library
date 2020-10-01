f54ce302fa1ea4baeb2ca3a126394067bd444254
EdwardLauv <17520901@gm.uit.edu.vn>
2020-10-01 11:53:06 +0700

    message

--- Library/Common/ImagesToVideoUtils.swift
+++ Library/Common/ImagesToVideoUtils.swift
@@ -2,57 +2,43 @@ import Foundation
 import AVFoundation
 import UIKit
 
-typealias CXEMovieMakerCompletion = (URL) -> Void
-typealias CXEMovieMakerUIImageExtractor = (AnyObject) -> UIImage?
-
-
 public class ImagesToVideoUtils: NSObject {
     
     static let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
     static let tempPath = paths[0] + "/exprotvideo.mp4"
     static let fileURL = URL(fileURLWithPath: tempPath)
-    
-    
     var assetWriter:AVAssetWriter!
     var writeInput:AVAssetWriterInput!
     var bufferAdapter:AVAssetWriterInputPixelBufferAdaptor!
     var videoSettings:[String : Any]!
     var frameTime:CMTime!
     
-    var completionBlock: CXEMovieMakerCompletion?
-    var movieMakerUIImageExtractor:CXEMovieMakerUIImageExtractor?
-    
     public init(videoSettings: [String: Any]) {
         super.init()
         
-
+        
         if(FileManager.default.fileExists(atPath: ImagesToVideoUtils.tempPath)){
             guard (try? FileManager.default.removeItem(atPath: ImagesToVideoUtils.tempPath)) != nil else {
                 print("remove path failed")
                 return
             }
         }
-        
-        
         self.assetWriter = try! AVAssetWriter(url: ImagesToVideoUtils.fileURL, fileType: AVFileType.mov)
-        
         self.videoSettings = videoSettings
         self.writeInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: videoSettings)
         assert(self.assetWriter.canAdd(self.writeInput), "add failed")
-        
         self.assetWriter.add(self.writeInput)
         let bufferAttributes:[String: Any] = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32ARGB)]
         self.bufferAdapter = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: self.writeInput, sourcePixelBufferAttributes: bufferAttributes)
         self.frameTime = CMTimeMake(value: Int64(1), timescale: 1)
     }
     
-    func createMovieFrom(images: [UIImage], withCompletion: @escaping CXEMovieMakerCompletion){
+    func createMovieFrom(images: [UIImage], withCompletion: @escaping (URL) -> Void){
         self.createMovieFromSource(images: images, extractor: {(inputObject:AnyObject) -> UIImage? in
             return inputObject as? UIImage}, withCompletion: withCompletion)
     }
     
-    func createMovieFromSource(images: [AnyObject], extractor: @escaping CXEMovieMakerUIImageExtractor, withCompletion: @escaping CXEMovieMakerCompletion){
-        self.completionBlock = withCompletion
+    func createMovieFromSource(images: [AnyObject], extractor: @escaping (AnyObject) -> UIImage?, withCompletion: @escaping (URL) -> Void){
         
         self.assetWriter.startWriting()
         self.assetWriter.startSession(atSourceTime: CMTime.zero)
@@ -61,20 +47,19 @@ public class ImagesToVideoUtils: NSObject {
         var i = 0
         let frameNumber = images.count
         
+        // add image
         self.writeInput.requestMediaDataWhenReady(on: mediaInputQueue){
             while(true){
-                if(i >= frameNumber){
+                if(i >= frameNumber){ // check number if more than frame number is break
                     break
                 }
-                
-                if (self.writeInput.isReadyForMoreMediaData){
+                if (self.writeInput.isReadyForMoreMediaData){ // check if writer input is ready
                     var sampleBuffer:CVPixelBuffer?
                     autoreleasepool{
                         let img = extractor(images[i])
                         if img == nil{
                             i += 1
                             print("Warning: counld not extract one of the frames")
-                            //continue
                         }
                         sampleBuffer = self.newPixelBufferFrom(cgImage: img!.cgImage!)
                     }
@@ -94,20 +79,27 @@ public class ImagesToVideoUtils: NSObject {
             self.writeInput.markAsFinished()
             self.assetWriter.finishWriting {
                 DispatchQueue.main.sync {
-                    self.completionBlock!(ImagesToVideoUtils.fileURL)
+                    withCompletion(ImagesToVideoUtils.fileURL)
                 }
             }
         }
     }
     
-    func newPixelBufferFrom(cgImage:CGImage) -> CVPixelBuffer?{
-        let options:[String: Any] = [kCVPixelBufferCGImageCompatibilityKey as String: true, kCVPixelBufferCGBitmapContextCompatibilityKey as String: true]
+    func newPixelBufferFrom(cgImage:CGImage) -> CVPixelBuffer? {
+        // option pixel buffer image
+        let options:[CFString: Any] = [
+            kCVPixelBufferCGImageCompatibilityKey : true,
+            kCVPixelBufferCGBitmapContextCompatibilityKey : true]
+        
         var pxbuffer:CVPixelBuffer?
         let frameWidth = cgImage.width
         let frameHeight = cgImage.height
         
+        
+        // check buffer
         let status = CVPixelBufferCreate(kCFAllocatorDefault, frameWidth, frameHeight, kCVPixelFormatType_32ARGB, options as CFDictionary?, &pxbuffer)
         assert(status == kCVReturnSuccess && pxbuffer != nil, "newPixelBuffer failed")
+        
         
         CVPixelBufferLockBaseAddress(pxbuffer!, CVPixelBufferLockFlags(rawValue: 0))
         let pxdata = CVPixelBufferGetBaseAddress(pxbuffer!)
@@ -115,7 +107,10 @@ public class ImagesToVideoUtils: NSObject {
         let context = CGContext(data: pxdata, width: frameWidth, height: frameHeight, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pxbuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
         assert(context != nil, "context is nil")
         
+        // create CGBitmapContext
         context!.concatenate(CGAffineTransform.identity)
+        
+        // Draw image into context
         context!.draw(cgImage, in: CGRect(x: 0, y: 0, width: cgImage.width, height: cgImage.height))
         CVPixelBufferUnlockBaseAddress(pxbuffer!, CVPixelBufferLockFlags(rawValue: 0))
         return pxbuffer
